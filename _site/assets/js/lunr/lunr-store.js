1d872f90caf1a64cca47bcbf2b2ea18957ba04c4
wally1002 <sankeerthreddy1002@gmail.com>
2021-07-12 19:35:46 +0530

    added sample post and configured

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Move the turtle",
+        "excerpt":"Move the turtle In the previous post we controlled the turtle using our keys, but we may want to automate this process by writing a piece of code which tells the turtle to move in a specified manner. There are 2 ways of controlling it using code. One is through...","categories": ["tech"],
+        "tags": ["Python","Robotics","ROS"],
+        "url": "/myBlog/tech/Move-the-turtle/",
+        "teaser": "/myBlog/assets/images/bat.jpg"
+      }]
