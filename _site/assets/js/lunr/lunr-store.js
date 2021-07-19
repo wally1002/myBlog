122881cf7cbbbd153af0940a04bdddf1785a8322
wally1002 <sankeerthreddy1002@gmail.com>
2021-07-19 13:19:02 +0530

    added side nav bars

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
+        "title": "ROS",
+        "excerpt":"Robotics Introduction Robotics is a very interesting word in itself. When you google Robot it says “Machine resembling human or replicating some human functions”. We human’s are ourselves very complex both biologically and psychologically. Yet we want to replicate something we don’t fully understand yet. For me this is where...","categories": ["tech"],
+        "tags": ["Python","Robotics","ROS"],
+        "url": "/myBlog/tech/ROS/",
+        "teaser": "/myBlog/assets/images/bat.jpg"
+      },{
+        "title": "Turtlesim",
+        "excerpt":"Turtlesim We now understand the core ideas discussed in the previous post using a 2D simulator called Turtlesim. We will explore the ideas of topics, messages and nodes. Let’s get started. Getting started with Turtlesim Turtlesim is a package which is included in ROS. So to use the Turtlesim we...","categories": ["tech"],
+        "tags": ["Python","Robotics","ROS"],
+        "url": "/myBlog/tech/Turtlesim/",
+        "teaser": "/myBlog/assets/images/bat.jpg"
+      },{
         "title": "Move the turtle",
         "excerpt":"Move the turtle In the previous post we controlled the turtle using our keys, but we may want to automate this process by writing a piece of code which tells the turtle to move in a specified manner. There are 2 ways of controlling it using code. One is through...","categories": ["tech"],
         "tags": ["Python","Robotics","ROS"],
-        "url": "/myBlog/tech/Move-the-turtle/",
+        "url": "/myBlog/tech/Movetheturtle/",
         "teaser": "/myBlog/assets/images/bat.jpg"
       }]
